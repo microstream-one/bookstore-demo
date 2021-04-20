5079112e7b951a487d769f50417b73b9d16f79fe
fh-ms <f.habermann@microstream.one>
2021-04-20 09:48:33 +0200

    Update dependencies

--- pnpmfile.js
+++ pnpmfile.js
@@ -8,8 +8,6 @@
 
 const fs = require('fs');
 
-const packageJson = JSON.parse(fs.readFileSync('./package.json', 'utf-8'));
-
 const versionsFile = require('path').resolve(__dirname, 'target/frontend/versions.json');
 
 if (!fs.existsSync(versionsFile)) {
@@ -25,13 +23,19 @@ module.exports = {
 
 function readPackage(pkg) {
   const {dependencies} = pkg;
-
+  
   if (dependencies) {
     for (let k in versions) {
       if (dependencies[k] && dependencies[k] !== versions[k]) {
         pkg.dependencies[k] = versions[k];
       }
     }
+  }
+
+  // Forcing chokidar version for now until new babel version is available
+  // check out https://github.com/babel/babel/issues/11488
+  if (pkg.dependencies.chokidar) {
+    pkg.dependencies.chokidar = '^3.4.0';
   }
 
   return pkg;
